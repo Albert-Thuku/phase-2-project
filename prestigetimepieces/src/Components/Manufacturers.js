66a36a6fccf46e4a662b1e3752680d560cdb24d1
Albert Thuku <athuku02@gmail.com>
2023-05-02 12:42:59 +0300

    Added Manufacturers.js file to filter watches displayed

---
+++ prestigetimepieces/src/Components/Manufacturers.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+function Manufacturers({watchData}){
+
+    const [manufacturerList, setManufacturerList] = useState([])
+    const [selectedManufacturer, setSelectedManufacturer] = useState(null)
+
+    watchData.forEach((watch)=>{
+        if(!manufacturerList.includes(watch.manufacturer)){
+            setManufacturerList([...manufacturerList, watch.manufacturer])
+        }
+    })
+
+    /* Add set function from parent component to pass data back up  */
+    function manufacturerSelect(manufacturer){
+        if(manufacturer){
+            setSelectedManufacturer(manufacturer)
+        }else{
+            setSelectedManufacturer(null)
+        }
+    }
+
+    return (
+        <div className="ManufacturerDiv">
+            <button onClick={() => manufacturerSelect(null)}>All</button>
+            {manufacturerList.map((manufacturer)=>{
+                return (
+                    <button 
+                        onClick={() => manufacturerSelect(manufacturer)} 
+                        key={manufacturer}
+                        className={selectedManufacturer === manufacturer ? 'active' : ''}
+                    >
+                        {manufacturer}
+                    </button>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Manufacturers;
