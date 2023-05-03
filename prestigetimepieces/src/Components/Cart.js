e4e1778a7b4d8ffdb54ec85bf0aaadd74b443476
Albert Thuku <athuku02@gmail.com>
2023-05-03 11:24:01 +0300

    Added transfer of data into cart and display of items added to cart

--- prestigetimepieces/src/Components/Cart.js
+++ prestigetimepieces/src/Components/Cart.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Cart(){
+function Cart({addedWatches}){
+
+    let [count, setCount] = useState(1)
+
+    function handleCount(e){
+        if(e.target.textContent === "+"){
+            setCount(count+=1);
+        }else{
+            if(count>0){
+            setCount(count-=1)
+            }
+        }
+    }
+
+    return (
+        <div id="cart-div">
+            <div id="cart-list">
+                {addedWatches.map((watch)=>{
+                    return (<div className="cart-item" key={watch.id}>
+                        <section className="cart-image">
+                            <img src={watch.image} alt="cart item" />
+                        </section>
+                        <p className="cart-details">
+                            {watch.name} <br />
+                            {watch.price} <br />
+                        </p>
+                        <button onClick={handleCount}>-</button>{count} <button onClick={handleCount}>+</button>
+                    </div>);
+                })}
+            </div>
+        </div>
+    );
 
 }
 
